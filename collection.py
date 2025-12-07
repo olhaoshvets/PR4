615ccb8d99a7bb1c0a6abd979d023f5397e2bc73
olhashvets <olha.o.shvets@ukd.edu.ua>
2025-12-07 14:22:36 +0200

    1

--- collection.py
+++ collection.py
@@ -1,5 +1,4 @@
 #Task 1 Creating set
-from conditionOperators import addition
 
 numbers = {'pizza', 'burger', 'soup', 'salad', 'pasta'}
 print(numbers)
@@ -41,5 +40,47 @@ print(checkSubset1, checkSubset2)
 mission = {'шпигунство', 'рятування', 'шахрайство'}
 legal = {'шпигунство', 'оберігання'}
 
-dif = mission.difference(legal)
-print(dif)
+mission = mission.intersection(legal)
+print(mission)
+
+# dif = mission.difference(legal)
+# for illegal in dif:
+#     if illegal in mission:
+#         mission.remove(illegal)
+
+#Task 4 Creating tuple
+numbers = (1, 2, 3, 4, 5)
+print(numbers[1])
+print(numbers[0], numbers[-1])
+print(numbers[:2])
+print(len(numbers))
+
+#Task 5 Changing tuple
+numbers = (1, 2, 3, 4, 5, -5, 12, 5, 42, 34)
+
+numbers = list(numbers)
+numbers.append(-56)
+numbers = tuple(numbers)
+print(numbers)
+
+numbers = list(numbers)
+numbers.remove(5)
+numbers = tuple(numbers)
+print(numbers)
+
+#Task 6 Join & unpacking
+fruits1 = ('apple', 'orange', 'banana', 'pineapple', 'cgerrys')
+fruits2 = ('mango', 'orange', 'lemon', '”', 'pear')
+num = (1, -6, 23, 45)
+
+fruits3 = fruits1 + fruits2
+print(fruits3)
+
+num1 = num * 3
+print(num1)
+
+(first, *rest, last1, last2) = fruits3
+
+print(first)
+print(last1, last2)
+print(rest)
